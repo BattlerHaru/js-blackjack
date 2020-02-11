9e143af49a6d07aa9bcd7a7fec83bde3d72772ef
Haru <Haru.Navi18@gmail.com>
2020-02-11 16:27:41 -0600

    se agrego la funcion de pedir carta, la cual añade al dom una img de la carta y muestra los puntos correspondientes, al llegar a 21 o mas se bloquea el boton de pedir carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,8 +1,15 @@
 let deck = [];
-
 const tipos = ['C', 'D', 'H', 'S'];
-
 const especiales = ['A', 'J', 'Q', 'K'];
+
+const btnPedir = document.querySelector('#btnPedir');
+
+const divCartasJugador = document.querySelector('#jugador-cartas');
+
+let puntosJugador = 0,
+    puntosComputadora = 0;
+const puntosHTML = document.querySelector('small');
+
 
 const crearDeck = () => {
     for (let i = 2; i <= 10; i++) {
@@ -18,25 +25,19 @@ const crearDeck = () => {
     }
 
     deck = _.shuffle(deck);
-
     return deck;
 }
 
 crearDeck();
 
-console.log(deck);
-
 const pedirCarta = () => {
-        if (deck.length === 0) {
-            throw 'No hay cartas en el deck'
-        }
-
-        const carta = deck.pop();
-
-        return carta;
-
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck'
     }
-    //pedirCarta();
+
+    const carta = deck.pop();
+    return carta;
+}
 
 const valorCarta = (carta) => {
     const valor = carta.substring(0, carta.length - 1);
@@ -46,4 +47,22 @@ const valorCarta = (carta) => {
         valor * 1;
 }
 
-valorCarta('AD');
+btnPedir.addEventListener('click', () => {
+    const carta = pedirCarta();
+
+    puntosJugador = puntosJugador + valorCarta(carta);
+    puntosHTML.innerText = puntosJugador;
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/img/${ carta }.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append(imgCarta);
+
+    if (puntosJugador > 21) {
+        console.warn('Buuuu Perdiste');
+        btnPedir.disabled = 'true';
+    } else if (puntosJugador === 21) {
+        console.log('%c Ganaste!!', 'background: #ce910e; color: white;');
+        btnPedir.disabled = 'true';
+    }
+});
