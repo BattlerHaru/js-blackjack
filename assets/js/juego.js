32cd76a149b8ffaab85e2a2e3848858c8d4b0e3f
Haru <Haru.Navi18@gmail.com>
2020-02-11 15:20:24 -0600

    se agrego la funcion del valor de la carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -27,17 +27,30 @@ crearDeck();
 console.log(deck);
 
 const pedirCarta = () => {
-    if (deck.length === 0) {
-        throw 'No hay cartas en el deck'
+        if (deck.length === 0) {
+            throw 'No hay cartas en el deck'
+        }
+
+        const carta = deck.pop();
+
+        return carta;
+
+    }
+    //pedirCarta();
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    let puntos = 0;
+
+    if (isNaN(valor)) {
+        puntos = (valor === 'A') ? 11 : 10;
+        console.log('no es un numero');
+    } else {
+        console.log('es un numero');
+        puntos = valor * 1;
     }
 
-    const carta = deck.pop();
-
-    console.log(carta);
-
-    return carta;
-
+    console.log(puntos);
 }
-pedirCarta();
 
-console.log(deck);
+valorCarta('AD');
